deba5d1e6e4b290f5e0f2a022b099eb60d4e5971
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-12-27 10:50:08 -0600

    ref div changes

--- main.js
+++ main.js
@@ -316,7 +316,7 @@ if (!window.customElements) {
   scrpt.src = museDir + 'webcomponents-lite.js';
   window.addEventListener('WebComponentsReady', function () {
     console.log('Webcomponents provided through polyfill.');
-    obtain([app, 'µ/components/refDiv.js'], (imports)=> {
+    obtain([app], (imports)=> {
       if (!started) {
         started = true;
         console.log(document.readyState);
@@ -330,7 +330,7 @@ if (!window.customElements) {
 
   document.head.insertBefore(scrpt, document.currentScript);
 } else {
-  obtain([app, 'µ/components/refDiv.js'], (imports)=> {
+  obtain([app], (imports)=> {
     if (!started) {
       started = true;
       if (document.readyState === 'complete' || document.readyState === 'loaded' || document.readyState === 'interactive') imports.app.start();
