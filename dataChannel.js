b6daaefc177ff4c1a28e3966c1519eac1afefe94
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-04-06 21:39:14 -0500

    back to basics

--- dataChannel.js
+++ dataChannel.js
@@ -8,7 +8,11 @@ obtain([], ()=> {
     var configuration = {
       iceServers: [{
         urls: 'stun:stun2.l.google.com:19302',
-      }, ],
+      }, {
+        url: 'turn:numb.viagenie.ca',
+        credential: 'muazkh',
+        username: 'webrtc@live.com',
+      },],
     };
 
     this.cnxn = new RTCPeerConnection(configuration);
