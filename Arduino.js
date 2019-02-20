3def813f57fe25e55ec04086e5379ae652e88e7b
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2019-02-20 11:19:47 -0600

    troubleshooting arduino

--- Arduino.js
+++ Arduino.js
@@ -1,4 +1,4 @@
-obtain(['events'], (EventEmitter)=> {
+obtain(['µ/serialParser.js', 'events'], ({ serialParser }, EventEmitter)=> {
   const DIGI_READ = 1;
   const ANA_READ = 2;
   const DIGI_WRITE = 4;
